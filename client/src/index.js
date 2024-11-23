444188e4a5ded5f2b574e82ab52c3c2a0e75bb78
sartaj193 <sartajsingh9jn6687@gmail.com>
2024-11-23 11:50:48 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { CartProvider } from "./context/cartcontext";
-import { AuthProvider } from "./context/authcontext";
+
 // Ensure AuthProvider is imported
 
 import App from "./App";
@@ -15,11 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
+    
           <App />
-        </CartProvider>
-      </AuthProvider>
+     
     </BrowserRouter>
   </React.StrictMode>
 );
